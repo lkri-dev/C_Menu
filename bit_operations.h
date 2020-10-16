9cb3b01c9d5faf06ef256aab1b75c30034a8ace6
lkri <lkri@techcollege.dk>
2020-10-16 08:08:01 +0200

    added bit check to bit_operations.h

--- bit_operations.h
+++ bit_operations.h
@@ -3,19 +3,40 @@
 /*
 Example for bit operations in C.
 */
+bool bit_check_operation (int check, int bit) {
+	if (bit == 0) return check == 0;
+	bool bit_check = (1 << bit) == (check & (1 << bit));
+	return bit_check;
+}
 
 int shift (int a) {
+	bool check;
 	printf("Before any shift %d\n", a);
+	
 	a |= (1 << 6); //Set a bit
-	//printf("%u ",num&maxPow ? 1 : 0);
+	check = bit_check_operation(a, 6);
+	printf("check bit %s \n", check ? "true" : "false");
 	printf("After a |= (1 << 6); //Set a bit %d\n", a);
+	
 	a &= ~(1 << 6); //clear a bit
+	check = bit_check_operation(a, 0);
+	printf("check bit %s \n", check ? "true" : "false");
 	printf("After a &= ~(1 << 6); //clear a bit %d\n", a);
-    a ^= (1 << 4); //toggle bit
+    
+	a ^= (1 << 4); //toggle bit
+	check = bit_check_operation(a, 4);
+	printf("check bit %s \n", check ? "true" : "false");
 	printf("After a ^= (1 << 4); //toggle bit %d\n", a);
+	
 	a <<= 2; //left shift whold byte[]
+	check = bit_check_operation(a, 6);
+	printf("check bit %s \n", check ? "true" : "false");
 	printf("After a <<= 2; //left shift whole byte[] %d\n", a);
+	
 	a >>= 2; //right shift whold byte[]
+	check = bit_check_operation(a, 4);
+	printf("check bit %s \n", check ? "true" : "false");
 	printf("After a >>= 2; //right shift whole byte[] %d\n", a);
+	
 	return a;
 }
